cc9bcf9108baccdca189f7cee809e9fdda0997ed
Julián Velandia <jvelandiag@unal.edu.co>
2021-05-07 09:54:37 -0500

    .

--- cuestionario/admin.py
+++ cuestionario/admin.py
@@ -1,7 +1,8 @@
 from cuestionario.models import Pregunta
 from django.contrib import admin
-from .models import Pregunta, Cuestionario
+from .models import Pregunta, Cuestionario, Opcion
 
 # Register your models here.
 admin.site.register(Pregunta)
 admin.site.register(Cuestionario)
+admin.site.register(Opcion)
--- cuestionario/models.py
+++ cuestionario/models.py
@@ -2,16 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+
+class Cuestionario(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title 
+
+
 class Pregunta(models.Model):
     pregunta = models.CharField(max_length=100)
-
+    cuestionario_id = models.ForeignKey(Cuestionario, on_delete=models.CASCADE)   
 
     def __str__(self):
         return self.pregunta 
+
+class Opcion(models.Model):
+    opcion = models.CharField(max_length=100)
+    es_correcta = models.BooleanField()
+
+    pregunta_id = models.ForeignKey(Pregunta, on_delete=models.CASCADE)   
+
         
-class Cuestionario(models.Model):
-    title = models.CharField(max_length=30)
-    pregunta_id = models.ForeignKey(Pregunta, on_delete=models.CASCADE, blank=True, null=True,default=None)   
 
 
 
