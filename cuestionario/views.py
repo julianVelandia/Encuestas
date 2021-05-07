d35e065cc31d21ce416a740795389b24eb183c7d
Julián Velandia <jvelandiag@unal.edu.co>
2021-05-07 16:59:49 -0500

    .

--- cuestionario/views.py
+++ cuestionario/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, CreateView,UpdateView,DeleteView
-
+from .models import Cuestionario, Pregunta, Opcion
 
 
 # Create your views here.
 class CrearView(CreateView):
+    model = Pregunta
+   
+    fields = ['pregunta','cuestionario_id']
     template_name = 'crear.html'
+    success_url = 'cuestionario/responder'
 
 class ResponderView(DetailView):
     template_name = 'responder.html'
